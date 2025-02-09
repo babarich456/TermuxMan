4a48406c8fee0cf374372d599731ec0309beaafa
Muhammet Atilla <muhammetcanaltan14@gmail.com>
2025-02-09 18:48:01 +0300

    Create menu.sh

---
+++ menu.sh
@@ -0,0 +1,19 @@
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+red='\e[1;31m'
+default='\e[0m'
+yellow='\e[0;33m'
+orange='\e[38;5;166m'
+green='\033[92m'
+
+clear
+sleep 1.5
+echo -e "$default"
+figlet " SETOOLMAN "
+1- NMAP
+2- GOBUSTER
+3- SETOOLKIT
+4- HYDRA
+5- METASPLOIT-FRAMEWORK
