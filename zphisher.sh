89d15e0d98280c43ccc0840888a22743dae75e12
Muhammet Atilla <muhammetcanaltan14@gmail.com>
2025-02-09 19:20:27 +0300

    Create zphisher.sh

---
+++ zphisher.sh
@@ -0,0 +1,19 @@
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+red='\e[1;31m'
+default='\e[0m'
+yellow='\e[0;33m'
+orange='\e[38;5;166m'
+green='\033[92m'
+
+clear
+sleep 1.5
+echo -e "$default"
+bash started.sh
+apt update
+apt upgrade
+pkg install tur-repo
+pkg install zphisher
+zphisher
