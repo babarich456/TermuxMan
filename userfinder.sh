61b126c4a2a37396cae55d30104d29828062e044
Muhammet Atilla <muhammetcanaltan14@gmail.com>
2025-02-09 19:23:16 +0300

    Create userfinder.sh

---
+++ userfinder.sh
@@ -0,0 +1,21 @@
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+red='\e[1;31m'
+default='\e[0m'
+yellow='\e[0;33m'
+orange='\e[38;5;166m'
+green='\033[92m'
+
+clear
+sleep 1.5
+echo -e "$default"
+apt update
+apt upgrade
+apt install git
+apt install curl
+apt install jq
+git clone https://github.com/mishakorzik/UserFinder
+cd UserFinder
+bash UserFinder.sh
